512378b2f78fb808535e76f0dca55984157fa9b2
Dawid Wolas <vrplayerek@gmail.com>
2022-01-21 18:06:25 +0100

    add image change

--- src/js/script.js
+++ src/js/script.js
@@ -86,6 +86,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -136,53 +137,66 @@
       console.log('formData',formData);
 
       // set price to default price
-  let price = thisProduct.data.price;
+      let price = thisProduct.data.price;
 
-  // for every category (param)...
-  for(let paramId in thisProduct.data.params) {
-    // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
-    const param = thisProduct.data.params[paramId];
-    console.log(paramId, param);
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+      // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
     
 
-    // for every option in this category
-    for(let optionId in param.options) {
-      // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
-      const option = param.options[optionId];
-      console.log(optionId, option);
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          const checkDefault =option.hasOwnProperty('default');
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if(optionSelected) {
+          // check if the option is not default
+            if(checkDefault == false){
+            // add option price to price variable
+              price += option.price;
 
-      const checkDefault =optionId.hasOwnProperty('default');
-      // check if there is param with a name of paramId in formData and if it includes optionId
-  const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
-    // check if the option is not default
-    if(optionSelected) {
-      // add option price to price variable
-      if(checkDefault == false){
-
-        price += option.price;
-      }
+            }
     
-   }else { if(checkDefault == true){
-    // check if the option is default
+            else { if(checkDefault == true){
+              // check if the option is default
      
-      // reduce price variable
-      price -= option.price;
-    
-  }
+              // reduce price variable
+              price -= option.price;
+              
+            }
+            const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
 
-}
-    
-  }
+            // check if there is an image
+            if(optionImage){
 
-  // update calculated price in the HTML
-  thisProduct.priceElem.innerHTML = price;
-}
+              // check if the option is selected
+              if(optionSelected){
+
+                // if yes add class active (image)
+                optionImage.classList.add(classNames.menuProduct.imageVisible);
+
+                // if not remove class active (image)
+              } else  {
+                optionImage.classList.remove(classNames.menuProduct.imageVisible);
+              }
+            }
+    
+            } 
+          }
+          // update calculated price in the HTML
+          thisProduct.priceElem.innerHTML = price;
+        }
 
       
-    }
+      }
   
+    }
   }
-    
   
   const app = {
     initData: function(){
